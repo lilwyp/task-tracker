ff71d2c0c065627c9128307c40b29043717c95f5
lilwyp <lilwyp@yandex.ru>
2024-10-02 18:05:03 +0500

    minor fixes localStorage

--- script.js
+++ script.js
@@ -5,73 +5,123 @@ window.onload = function () {
 };
 
 // Функция для создания таска, как нового, так и отрисовки тех, которые хранятся в localStorage
-function addTask(newBool, taskHeadContent, taskTextContent) {
-  const taskDiv = document.createElement("div");
-  taskDiv.classList.add("task");
-  taskDiv.id = "task-" + taskId;
+function addTask(newBool, savedTaskId, savedTaskHead, savedTaskText) {
+  if (newBool == true) {
+    const taskDiv = document.createElement("div");
+    taskDiv.classList.add("task");
+    taskDiv.id = "task-" + taskId;
 
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  checkbox.classList.add("task-checkbox");
-  checkbox.id = "task-checkbox-" + taskId;
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add("task-checkbox");
+    checkbox.id = "task-checkbox-" + taskId;
 
-  const contentDiv = document.createElement("div");
-  contentDiv.classList.add("task-content");
+    const contentDiv = document.createElement("div");
+    contentDiv.classList.add("task-content");
 
-  const taskHead = document.createElement("h2");
-  taskHead.classList.add("task-head");
-  taskHead.id = "task-head-" + taskId;
-  if (taskHeadContent != "") {
-    taskHead.textContent = taskHeadContent;
-  } else {
+    const taskHead = document.createElement("h2");
+    taskHead.classList.add("task-head");
+    taskHead.id = "task-head-" + taskId;
     taskHead.textContent = "Your task head";
-  }
 
-  const taskText = document.createElement("p");
-  taskText.classList.add("task-text");
-  taskText.id = "task-text-" + taskId;
-  if (taskTextContent != "") {
-    taskText.textContent = taskTextContent;
-  } else {
+    const taskText = document.createElement("p");
+    taskText.classList.add("task-text");
+    taskText.id = "task-text-" + taskId;
     taskText.textContent = "Your task text";
+
+    const btnsDiv = document.createElement("div");
+    btnsDiv.classList.add("task-btns");
+
+    const deleteBtn = document.createElement("input");
+    deleteBtn.type = "button";
+    deleteBtn.value = "delete";
+    deleteBtn.classList.add("task-btn");
+    deleteBtn.id = "task-btn-delete-" + taskId;
+    deleteBtn.addEventListener("click", function () {
+      taskDelete(taskDiv.id);
+    });
+
+    const editBtn = document.createElement("input");
+    editBtn.type = "button";
+    editBtn.value = "edit";
+    editBtn.classList.add("task-btn");
+    editBtn.id = "task-btn-edit-" + taskId;
+    editBtn.addEventListener("click", function () {
+      taskEdit(taskHead.id, taskText.id, editBtn.id);
+    });
+
+    contentDiv.appendChild(taskHead);
+    contentDiv.appendChild(taskText);
+    taskDiv.appendChild(checkbox);
+    taskDiv.appendChild(contentDiv);
+    btnsDiv.appendChild(deleteBtn);
+    btnsDiv.appendChild(editBtn);
+    taskDiv.appendChild(btnsDiv);
+
+    document.getElementById("tasks-container").appendChild(taskDiv);
+
+    saveTaskToLocalStorage(
+      taskId,
+      taskHead.textContent,
+      taskText.textContent,
+      false
+    );
+    taskId++;
+  } else if (newBool == false) {
+    savedTaskIdNumber = savedTaskId.replace(/\D/g, "");
+    const taskDiv = document.createElement("div");
+    taskDiv.classList.add("task");
+    taskDiv.id = savedTaskId;
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add("task-checkbox");
+    checkbox.id = "task-checkbox-" + savedTaskIdNumber;
+
+    const contentDiv = document.createElement("div");
+    contentDiv.classList.add("task-content");
+
+    const taskHead = document.createElement("h2");
+    taskHead.classList.add("task-head");
+    taskHead.id = "task-head-" + savedTaskIdNumber;
+    taskHead.textContent = savedTaskHead;
+
+    const taskText = document.createElement("p");
+    taskText.classList.add("task-text");
+    taskText.id = "task-text-" + savedTaskIdNumber;
+    taskText.textContent = savedTaskText;
+
+    const btnsDiv = document.createElement("div");
+    btnsDiv.classList.add("task-btns");
+
+    const deleteBtn = document.createElement("input");
+    deleteBtn.type = "button";
+    deleteBtn.value = "delete";
+    deleteBtn.classList.add("task-btn");
+    deleteBtn.id = "task-btn-delete-" + savedTaskIdNumber;
+    deleteBtn.addEventListener("click", function () {
+      taskDelete(taskDiv.id);
+    });
+
+    const editBtn = document.createElement("input");
+    editBtn.type = "button";
+    editBtn.value = "edit";
+    editBtn.classList.add("task-btn");
+    editBtn.id = "task-btn-edit-" + savedTaskIdNumber;
+    editBtn.addEventListener("click", function () {
+      taskEdit(taskHead.id, taskText.id, editBtn.id);
+    });
+
+    contentDiv.appendChild(taskHead);
+    contentDiv.appendChild(taskText);
+    taskDiv.appendChild(checkbox);
+    taskDiv.appendChild(contentDiv);
+    btnsDiv.appendChild(deleteBtn);
+    btnsDiv.appendChild(editBtn);
+    taskDiv.appendChild(btnsDiv);
+
+    document.getElementById("tasks-container").appendChild(taskDiv);
   }
-
-  const btnsDiv = document.createElement("div");
-  btnsDiv.classList.add("task-btns");
-
-  const deleteBtn = document.createElement("input");
-  deleteBtn.type = "button";
-  deleteBtn.value = "delete";
-  deleteBtn.classList.add("task-btn");
-  deleteBtn.id = "task-btn-delete-" + taskId;
-  deleteBtn.addEventListener("click", function () {
-    taskDelete(taskDiv.id);
-  });
-
-  const editBtn = document.createElement("input");
-  editBtn.type = "button";
-  editBtn.value = "edit";
-  editBtn.classList.add("task-btn");
-  editBtn.id = "task-btn-edit-" + taskId;
-  editBtn.addEventListener("click", function () {
-    taskEdit(taskHead.id, taskText.id, editBtn.id);
-  });
-
-  contentDiv.appendChild(taskHead);
-  contentDiv.appendChild(taskText);
-  taskDiv.appendChild(checkbox);
-  taskDiv.appendChild(contentDiv);
-  btnsDiv.appendChild(deleteBtn);
-  btnsDiv.appendChild(editBtn);
-  taskDiv.appendChild(btnsDiv);
-
-  document.getElementById("tasks-container").appendChild(taskDiv);
-
-  if (newBool) {
-    saveTaskToLocalStorage(taskId, taskHead.textContent, taskText.textContent);
-  }
-
-  taskId++;
 }
 
 // Функция для удаления задачи
@@ -79,11 +129,19 @@ function taskDelete(id) {
   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   let newTasks = [];
   tasks.forEach(function (task) {
-    if (task.savedTaskId != id) {
+    if (task.savedTaskId == id) {
       newTasks.push({
         savedTaskId: task.savedTaskId,
         savedTaskHead: task.savedTaskHead,
         savedTaskText: task.savedTaskText,
+        savedTaskDeleted: true,
+      });
+    } else {
+      newTasks.push({
+        savedTaskId: task.savedTaskId,
+        savedTaskHead: task.savedTaskHead,
+        savedTaskText: task.savedTaskText,
+        savedTaskDeleted: task.savedTaskDeleted,
       });
     }
   });
@@ -107,17 +165,17 @@ function taskEdit(headId, textId, editBtnId) {
     tasks.forEach(function (task) {
       if (task.savedTaskId == "task-" + headId.replace(/\D/g, "")) {
         newTasks.push({
-          savedTaskId: document.getElementById(
-            "task-" + headId.replace(/\D/g, "")
-          ),
+          savedTaskId: "task-" + headId.replace(/\D/g, ""),
           savedTaskHead: document.getElementById(headId).textContent,
           savedTaskText: document.getElementById(textId).textContent,
+          savedTaskDeleted: task.savedTaskDeleted,
         });
       } else {
         newTasks.push({
           savedTaskId: task.savedTaskId,
           savedTaskHead: task.savedTaskHead,
           savedTaskText: task.savedTaskText,
+          savedTaskDeleted: task.savedTaskDeleted,
         });
       }
     });
@@ -133,12 +191,18 @@ function taskEdit(headId, textId, editBtnId) {
 }
 
 // Функция для сохранения задачи в localStorage
-function saveTaskToLocalStorage(taskIdSaved, taskHeadSaved, taskTextSaved) {
+function saveTaskToLocalStorage(
+  taskIdSaved,
+  taskHeadSaved,
+  taskTextSaved,
+  taskDeletedSaved
+) {
   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   tasks.push({
     savedTaskId: "task-" + taskIdSaved,
     savedTaskHead: taskHeadSaved,
     savedTaskText: taskTextSaved,
+    savedTaskDeleted: taskDeletedSaved,
   });
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
@@ -147,11 +211,14 @@ function saveTaskToLocalStorage(taskIdSaved, taskHeadSaved, taskTextSaved) {
 function loadTask() {
   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   tasks.forEach(function (task) {
-    addTask(
-      (newBool = false),
-      (taskHeadContent = task.savedTaskHead),
-      (taskTextContent = task.savedTaskText)
-    );
+    if (task.savedTaskDeleted != true) {
+      addTask(
+        (newBool = false),
+        (savedTaskId = task.savedTaskId),
+        (taskHeadContent = task.savedTaskHead),
+        (taskTextContent = task.savedTaskText)
+      );
+    }
   });
   taskId = tasks.length + 1;
 }
